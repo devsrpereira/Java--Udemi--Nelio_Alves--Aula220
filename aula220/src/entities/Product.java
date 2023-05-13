b40ab61bf2e2d0d1d921431191846cf802339c8d
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-05-12 22:34:23 -0300

    pakage and inital class

---
+++ aula220/src/entities/Product.java
@@ -0,0 +1,39 @@
+package entities;
+
+public class Product {
+    private String name;
+    private double price;
+    private int unity;
+
+    public Product() {
+
+    }
+    public Product(String name, double price, int unity) {
+        this.name = name;
+        this.price = price;
+        this.unity = unity;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getUnity() {
+        return unity;
+    }
+    public void setUnity(int unity) {
+        this.unity = unity;
+    }
+
+
+
+}
